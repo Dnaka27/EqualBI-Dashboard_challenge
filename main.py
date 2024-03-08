5d18cba2f5a4ce47dde77b1c2fdee31de1fc477e
Diogo Oike <diogooikekanefukujapan@gmail.com>
2024-03-08 06:01:06 -0300

    Plotly try

--- main.py
+++ main.py
@@ -12,15 +12,6 @@ df_fam_prod_venda = load_data("df_fam_prod_venda.xlsx")
 df_fam_prod_venda_timed = load_data("df_fam_prod_venda_timed.xlsx")
 df_saleBranch = load_data("df_saleBranch.xlsx")
 
-df_fam_prod_venda_mut = df_fam_prod_venda
-
-KEY_COUNTER = 0
-
-def get_unique_key(prefix="widget"):
-    global KEY_COUNTER
-    KEY_COUNTER += 1
-    return f"{prefix}_{KEY_COUNTER}"
-
 with st.container():
     st.title("Dashboard gerencial")
 
@@ -37,24 +28,19 @@ if selected == "Produtos":
         
     with st.container():   
         st.write("")
-        df_fam_prod_venda_mut["descricao_codigo"] = df_fam_prod_venda_mut.apply(lambda x: f"{x['codigo_familia']} - {x['descricaofamilia']}", axis=1)
+        df_fam_prod_venda["descricao_codigo"] = df_fam_prod_venda.apply(lambda x: f"{x['codigo_familia']} - {x['descricaofamilia']}", axis=1)
         
-        df_fam_prod_venda_group = df_fam_prod_venda_mut.groupby(['descricao_codigo', 'codigo_familia'])['lucro_produto'].sum().reset_index()
+        df_fam_prod_venda_group = df_fam_prod_venda.groupby(['descricao_codigo', 'codigo_familia'])['lucro_produto'].sum().reset_index()
         
         number_filter_fam = int(st.slider("Famílias de produtos a visualizar: ", 1, 75, 7))
-        st.markdown("---")
-        df_fam_prod_venda_order = st.selectbox("Famílias ordenadas por lucro: ", ["Decrescente", "Crescente"], key=get_unique_key())
+        st.write("---")
+        df_fam_prod_venda_order = st.selectbox("Famílias ordenadas por lucro: ", ["Decrescente", "Crescente"])
         
         df_fam_prod_venda_group = df_fam_prod_venda_group.sort_values(by='lucro_produto', ascending=(df_fam_prod_venda_order == "Crescente")).reset_index(drop=True)
             
         df_fam_prod_venda_filter_order = df_fam_prod_venda_group.head(number_filter_fam) 
         df_fam_prod_venda_filter_order["Codigo - Descrição"] = df_fam_prod_venda_filter_order["descricao_codigo"]
         df_fam_prod_venda_filter_order["Lucro por família"] = df_fam_prod_venda_filter_order["lucro_produto"]
-        
-        fig_order_by_fam_prod_venda = go.Figure()
-        fig_order_by_fam_prod_venda = go.Figure(data=[go.Bar(x=df_fam_prod_venda_filter_order['descricao_codigo'], y=df_fam_prod_venda_filter_order['lucro_produto'])])
-        fig_order_by_fam_prod_venda.update_layout(xaxis=dict(tickmode='linear'), yaxis=dict(title='LUCRO POR FAMÍLIA',  titlefont=dict(color='rgb(0,210,0)', size=15, family='Montserrat, sans-serif')))
-        st.plotly_chart(fig_order_by_fam_prod_venda)
         
         st.bar_chart(df_fam_prod_venda_filter_order, x="Codigo - Descrição", y="Lucro por família")
         
@@ -75,7 +61,7 @@ if selected == "Produtos":
                 text_prodOrder2 = "Produtos que menos vendem"
     
             st.markdown("Produtos: ")
-            df_fam_prod_venda_filter_order_search = df_fam_prod_venda_mut.groupby("codigo_familia")[role].sum().reset_index()
+            df_fam_prod_venda_filter_order_search = df_fam_prod_venda.groupby("codigo_familia")[role].sum().reset_index()
             
             fam_search_filter, prod_search_filter = st.columns(2)
             with fam_search_filter:
@@ -89,25 +75,17 @@ if selected == "Produtos":
             df_fam_prod_venda_filter_order_search = df_fam_prod_venda_filter_order_search.sort_values(by=role, ascending=(df_fam_prod_venda_order_fam_search == text_famOrder2)).reset_index(drop=True)
             df_fam_prod_venda_filter_order_search = df_fam_prod_venda_filter_order_search["codigo_familia"].head(number_filter_fam_search)
             
-            df_fam_prod_search_prod_top = pd.merge(df_fam_prod_venda_filter_order_search, df_fam_prod_venda_mut[["codigo_familia", role, "descricaoproduto", "descricao_codigo"]], on="codigo_familia", how="inner").sort_values(by=role, ascending=False).reset_index(drop=True)
+            df_fam_prod_search_prod_top = pd.merge(df_fam_prod_venda_filter_order_search, df_fam_prod_venda[["codigo_familia", role, "descricaoproduto", "descricao_codigo"]], on="codigo_familia", how="inner").sort_values(by=role, ascending=False).reset_index(drop=True)
             df_fam_prod_search_prod_top = df_fam_prod_search_prod_top.groupby('codigo_familia').apply(lambda x: x.nlargest(number_filter_prod_search, role).reset_index(drop=True) if df_fam_prod_venda_order_prod_search == text_prodOrder1 else x.nsmallest(number_filter_prod_search, role)).reset_index(drop=True)
 
             prod_a, prod_b = st.columns(2)
             
-            i = 0
-            for cod in df_fam_prod_venda_filter_order_search.to_list():
+            for i, cod in enumerate(df_fam_prod_venda_filter_order_search.to_list()):
                 df_fam_prod_search_prod_top_filter = df_fam_prod_search_prod_top[df_fam_prod_search_prod_top["codigo_familia"] == cod]
-                if i % 2 == 0:
-                    with prod_a:  
-                        st.write(f"Família: :green[{cod}]")
-                        st.bar_chart(df_fam_prod_search_prod_top_filter, x="descricaoproduto", y=role)
-                    i+=1
-                else:
-                    with prod_b:
-                        st.write(f"Família: :green[{cod}]")
-                        st.bar_chart(df_fam_prod_search_prod_top_filter, x="descricaoproduto", y=role)
-                    i+=1
-        
+                with (prod_a if i % 2 == 0 else prod_b):
+                    st.write(f"Família: :green[{cod}]")
+                    st.bar_chart(df_fam_prod_search_prod_top_filter, x="descricaoproduto", y=role)
+
         with st.expander("PRODUTOS POR FAMÍLIA"):
             render_by_role("lucro_produto")
                     
@@ -121,16 +99,16 @@ if selected == "Produtos":
             df_fam_prod_venda_timed["dif_fat"] = df_fam_prod_venda_timed.apply(calcular_dif_fat, axis=1)
             
             number_filter_fam_timed = int(st.slider("Famílias de produtos a visualizar: ", 1, 75, 5))
-            df_fam_prod_venda_order_timed = st.selectbox("Ordenação: ", ["Maiores faturamentos", "Menores faturamentos"], key=get_unique_key())
+            df_fam_prod_venda_order_timed = st.selectbox("Ordenação: ", ["Maiores faturamentos", "Menores faturamentos"])
             df_fam_most_sales = df_fam_prod_venda_group.sort_values(by='lucro_produto', ascending=(df_fam_prod_venda_order_timed == "Menores faturamentos")).reset_index(drop=True)
             
             df_fam_most_sales_timed = pd.merge(df_fam_most_sales.head(number_filter_fam_timed), df_fam_prod_venda_timed[["codigo_familia", "mes", "ano", "valor_monetario_total", "data", "dif_fat"]], on="codigo_familia", how="inner")
             
             for cod_fam in df_fam_most_sales["codigo_familia"].head(number_filter_fam_timed).to_list():
-                df_fam_most_sales_timed_filter = df_fam_most_sales_timed[df_fam_most_sales_timed["codigo_familia"] == cod_fam]
-                df_fam_most_sales_timed_filter_order = df_fam_most_sales_timed_filter.sort_values(by="data", ascending=True).reset_index(drop=True)
+                df_fam_most_sales_timed_filter_order = df_fam_most_sales_timed[df_fam_most_sales_timed["codigo_familia"] == cod_fam].sort_values(by="data", ascending=True).reset_index(drop=True)
                 st.write(f"Família: :green[{cod_fam}]")
                 st.line_chart(df_fam_most_sales_timed_filter_order, x="data", y="valor_monetario_total")
+
 
 if selected == "Filiais":
     with st.container():
@@ -147,7 +125,7 @@ if selected == "Filiais":
         with st.expander("CRESCIMENTO DE FATURAMENTO"):
             st.markdown("Famílias que mais cresceram/decresceram")
             number_filter_fam_timed_fat = int(st.slider("Famílias de produtos a visualizar: ", 1, 75, 4))
-            df_fam_prod_venda_order_timed_fat = st.selectbox("Ordenação: ", ["Famílias que mais cresceram", "Famílias que menos cresceram"], key=get_unique_key())
+            df_fam_prod_venda_order_timed_fat = st.selectbox("Ordenação: ", ["Famílias que mais cresceram", "Famílias que menos cresceram"])
             
             df_fam_prod_venda_timed["data"] = pd.to_datetime(df_fam_prod_venda_timed['ano'].astype(str) + '-' + df_fam_prod_venda_timed['mes'].astype(str))
             calcular_dif_fat = lambda row: row["valor_monetario_total"] - df_fam_prod_venda_timed.loc[df_fam_prod_venda_timed["data"].idxmin(), "valor_monetario_total"]
@@ -165,10 +143,9 @@ if selected == "Filiais":
             chosen_branch = st.multiselect("Filiais a visualisar: ", df_saleBranch_groupBranch["filial_venda"].to_list())
             
             for filial in chosen_branch:
-                df_filial_chosen = df_saleBranch_groupBranch_date[df_saleBranch_groupBranch_date["filial_venda"] == filial]
-                df_filial_chosen_order = df_filial_chosen.sort_values(by="data_venda", ascending=True).reset_index(drop=True)
+                df_filial_chosen = df_saleBranch_groupBranch_date[df_saleBranch_groupBranch_date["filial_venda"] == filial].sort_values(by="data_venda", ascending=True).reset_index(drop=True)
                 st.markdown(f"Filial N°: :green[{filial}]")
-                st.line_chart(df_filial_chosen_order, x="data_venda", y="valor_monetario_total")
+                st.line_chart(df_filial_chosen, x="data_venda", y="valor_monetario_total")
                 
         with st.expander("FATURAMENTO POR VENDEDOR"):
             number_filter_branchEmpl = int(st.slider("Vendedores a visualizar: ", 1, 50, 10))
@@ -177,31 +154,41 @@ if selected == "Filiais":
             
             df_saleBranch_groupEmpl = df_saleBranch.groupby(["codigo_vendedor", "nome_vendedor"])["valor_monetario_total"].sum().reset_index()
             df_saleBranch_groupEmpl["codigo_nome"] = df_saleBranch_groupEmpl.apply(lambda x: f"{x['codigo_vendedor']} - {x['nome_vendedor']}", axis=1)
-            df_saleBranch_groupEmpl = df_saleBranch_groupEmpl.sort_values(by="valor_monetario_total", ascending=(df_fam_prod_venda_order_empl == "Vendedores que menos venderam")).reset_index(drop=True)
-            df_saleBranch_groupEmpl = df_saleBranch_groupEmpl.head(number_filter_branchEmpl)
+            df_saleBranch_groupEmpl = df_saleBranch_groupEmpl.sort_values(by="valor_monetario_total", ascending=(df_fam_prod_venda_order_empl == "Vendedores que menos venderam")).reset_index(drop=True).head(number_filter_branchEmpl)
             st.bar_chart(df_saleBranch_groupEmpl, x="codigo_nome", y="valor_monetario_total")
 
 if selected == "Dados":
-    st.subheader("Dados utilizados: ")
-    st.markdown("#### :orange[Família, produtos e quantidade]")
-    st.write(df_fam_prod_venda_mut)
-    st.write("---")
-    st.markdown("#### :orange[Vendas de produtos pelo tempo]")
-    st.write(df_fam_prod_venda_timed)
-    st.write("---")
-    st.markdown("#### :orange[Vendedores e Filiais]")
-    st.write(df_saleBranch)
+    st.subheader(":orange[Dados utilizados: ]")
+    tab1, tab2, tab3 = st.tabs(["Família, produtos e quantidade", "Vendas de produtos pelo tempo", "Vendedores e Filiais"])
 
-    tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+    @st.cache_data
+    def convert_df(df):
+        return df.to_csv().encode('utf-8')
 
     with tab1:
-        st.header("A cat")
-        st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
-
+        st.write(df_fam_prod_venda)        
+        csv = convert_df(df_fam_prod_venda)
+        st.download_button(
+            label="Download data as CSV",
+            data=csv,
+            file_name='df_fam_prod_venda.csv',
+            mime='text/csv',
+        )
     with tab2:
-        st.header("A dog")
-        st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-
+        st.write(df_fam_prod_venda_timed)
+        csv = convert_df(df_fam_prod_venda_timed)
+        st.download_button(
+            label="Download data as CSV",
+            data=csv,
+            file_name='df_fam_prod_venda_timed.csv',
+            mime='text/csv',
+        )
     with tab3:
-        st.header("An owl")
-        st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+        st.write(df_saleBranch)
+        csv = convert_df(df_saleBranch)
+        st.download_button(
+            label="Download data as CSV",
+            data=csv,
+            file_name='df_saleBranch.csv',
+            mime='text/csv',
+        )
