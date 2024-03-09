1e42879b0db8a506db2b3ec5e6070f2d8844bc32
Diogo Oike <diogooikekanefukujapan@gmail.com>
2024-03-09 00:52:35 -0300

    Plotly test requirements

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from streamlit_option_menu import option_menu
+import plotly as pl
 
 st.set_page_config(page_title="Equal Dashboard", initial_sidebar_state="auto")
 
@@ -40,6 +41,11 @@ if selected == "Produtos":
         df_fam_prod_venda_filter_order = df_fam_prod_venda_group.head(number_filter_fam) 
         df_fam_prod_venda_filter_order["Codigo - Descrição"] = df_fam_prod_venda_filter_order["descricao_codigo"]
         df_fam_prod_venda_filter_order["Lucro por família"] = df_fam_prod_venda_filter_order["lucro_produto"]
+        
+        fig_order_by_fam_prod_venda = pl.graph_objs.Figure()
+        fig_order_by_fam_prod_venda = pl.graph_objs.Figure(data=[pl.graph_objs.Bar(x=df_fam_prod_venda_filter_order['descricao_codigo'], y=df_fam_prod_venda_filter_order['lucro_produto'])])
+        fig_order_by_fam_prod_venda.update_layout(xaxis=dict(tickmode='linear'), yaxis=dict(title='LUCRO POR FAMÍLIA',  titlefont=dict(color='rgb(0,210,0)', size=15, family='Montserrat, sans-serif')))
+        st.plotly_chart(fig_order_by_fam_prod_venda)
         
         st.bar_chart(df_fam_prod_venda_filter_order, x="Codigo - Descrição", y="Lucro por família")
         
